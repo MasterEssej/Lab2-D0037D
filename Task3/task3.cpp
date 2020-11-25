15b9c5d577391b9745a61861337e95e3571b0129
Jesse Viitanen <essej.viitanen@gmail.com>
2020-11-25 20:24:46 +0100

    task3

--- Task3/task3.cpp
+++ Task3/task3.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-void array_rows_cols(int arr[4][4]) {
+int x, y;
 
-	for (int i = 0; i < 2; ++i) {
-		for (int j = 0; j < 4; ++i) {
-			cout << arr[i][j] << ' ';
+void array_rows_cols(int arr[20][20]) {
+
+	for (int row = 0; row < x; row++) {
+		int sumx = 0;
+		for (int col = 0; col < y; col++) {
+			cout << " " << arr[row][col] << " ";
+			sumx += arr[row][col];
 		}
-		cout << endl;
+		cout << "|" << sumx << endl;
+	}
+	for (int col = 0; col < y; col++) {
+		cout << "---";
+	}
+	cout << endl;
+	
+	for (int col = 0; col < y; col++) {
+		int sumy = 0;
+		for (int row = 0; row < x; row++) {
+			sumy += arr[row][col];
+		}
+		cout << " " << sumy;
 	}
 
 }
 
 
+
 int main() {
-	int array1[][4] = { {1,2,3,4}, {5,6,7,8} };
+
+	int array1[20][20];
+
+	cout << "Number of rows (max 20): ";
+	cin >> x;
+
+	cout << "Number of columns (max 20): ";
+	cin >> y;
+
+	for (int rows = 0; rows < x; rows++) {
+		for (int cols = 0; cols < y; cols++) {
+			array1[rows][cols] = rand() % 10;
+		}
+	}
 
 	array_rows_cols(array1);
+
+	return 0;
 }
